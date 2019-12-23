8248b206bcc88704da089c1eed78827cbf270626
unknown <markus1abc@gmail.com>
2019-12-24 00:14:54 +0100

    Setup app for deployment on heroku

--- src/app.js
+++ src/app.js
@@ -7,6 +7,7 @@ const forecast = require('./utils/forecast')
 
 
 const app = express()
+const port = process.env.PORT || 3000
 
 
 //define paths for Express config
@@ -125,7 +126,7 @@ app.get('*', (req, res) => {
 
 
 
-app.listen(3000, () => {
-    console.log('Server is up on port 3000.')
+app.listen(port, () => {
+    console.log('Server is up on port '+ port)
 })
 
