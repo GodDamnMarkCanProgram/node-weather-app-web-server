55ed4efde0cc2282a6c847c45c15930e3168ddc1
unknown <markus1abc@gmail.com>
2019-12-24 00:59:50 +0100

    added info about windspeed and converted to celcius, wind speed is probably still in miles/hour darksky api unit option does not work

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,16 +1,19 @@
 const request = require('request')
+const fahrenheitToCelsius = require('fahrenheit-to-celsius')
 
 
 const forecast = (longditude, latitude, callback) =>{
-    const url = 'https://api.darksky.net/forecast/365e05c764f6bbc5e349e7c9cb40ec7f/' + longditude + ',' + latitude + '?units=si'
+    const url = 'https://api.darksky.net/forecast/365e05c764f6bbc5e349e7c9cb40ec7f/' + longditude + ',' + latitude + '?units=auto'
     request({url, json:true}, (error, {body}) => {
         if(error){
             callback('Unable to connect to service', undefined)
         }else if(body.error){
             callback('Unable to find location', undefined)
         }else{
-           
-            callback(undefined, body.daily.data[0].summary + '\nIt is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain')
+            const temperature = Math.round(fahrenheitToCelsius(body.currently.temperature));
+            console.log('Longditude: ' + longditude + 'latitude: ' + latitude)
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.'+
+            '\n Wind speeds are ' + body.currently.windSpeed + 'm/s')
         }
     })
 }
